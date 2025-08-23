14cbdef33b6a80f6d821325b1afb13d8676cb251
welson junior <eujuniorplus@gmail.com>
2025-08-22 21:17:01 -0300

    subindo estrutura completa do projeto

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template  >
+  <div>
+     <navbarMenu/>
+     <homeSection/>
+     <aboutUs/>
+     <teamLaw/>
+     <servicesLaw/>
+     <HelpCenter/>
+     <contactLaw/>
+     <rodapeLaw/>
+  </div>
+   
+  
+  
+</template>
+
+<script>
+import navbarMenu from './components/navbarMenu.vue';
+import homeSection from './components/homeSection.vue';
+import aboutUs from './components/aboutUs.vue';
+import teamLaw from './components/teamLaw.vue';
+import servicesLaw from './components/servicesLaw.vue';
+import HelpCenter from './components/HelpCenter.vue';
+import contactLaw from './components/contactLaw.vue';
+import rodapeLaw from './components/rodapeLaw.vue';
+
+export default {
+  name: 'App',
+  components: {
+    navbarMenu,
+    homeSection,
+    aboutUs,
+    teamLaw,
+    servicesLaw,
+    HelpCenter,
+   contactLaw,
+    rodapeLaw,
+  }
+}
+</script>
+
+
+<style>
+*{
+  margin: 0;
+  box-sizing: border-box;
+ font-family: "Cinzel", serif;
+  font-optical-sizing: auto;
+  font-weight: bold;
+  font-style: normal;
+  background-color:#212529;;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+import 'animate.css'
+
+
+
+import App from './App.vue'
+
+createApp(App).mount('#app')
